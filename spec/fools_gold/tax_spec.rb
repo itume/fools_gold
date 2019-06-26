b84576b252b834370dd8ff2c0986d57a7a56636a
itume <jem90ham@gmail.com>
2019-06-27 06:47:20 +0900

    separete yen test

---
+++ spec/fools_gold/tax_spec.rb
@@ -0,0 +1,54 @@
+RSpec.describe FoolsGold do
+  describe "Yen" do
+    describe "normal tax" do
+      let(:yen) {Yen.new(100)}
+      describe "with_tax?" do
+        subject {yen.with_tax?}
+        it "初期状態なら税抜きであること" do
+          is_expected.to be false
+        end
+
+        it "税込みに変更すると税込みになること" do
+          yen.with_tax!
+          is_expected.to be true
+        end
+      end
+
+      describe "without_tax?" do
+        subject {yen.without_tax?}
+        it "初期状態なら税抜きであること" do
+          is_expected.to be true
+        end
+
+        it "税込みに変更すると税込みになること" do
+          yen.with_tax!
+          is_expected.to be false
+        end
+      end
+
+      describe "with_tax!" do
+        it "初期状態なら課税できること" do
+          yen.with_tax!
+          expect(yen.with_tax?).to be true
+        end
+
+        it "課税状態に課税すると二重課税になること" do
+          yen.with_tax!
+          expect{yen.with_tax!}.to raise_error(DoubleTaxaionError)
+        end
+      end
+
+      describe "without_tax!" do
+        it "非課税の状態で税抜きにすると脱税になること" do
+          expect{yen.without_tax!}.to raise_error(TaxEvationError)
+        end
+
+        it "課税状態なら税抜きにできること" do
+          yen.with_tax!
+          yen.without_tax!
+          expect(yen.without_tax?).to be true
+        end
+      end
+    end
+  end
+end
