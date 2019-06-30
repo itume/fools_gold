8d870ceeefa3c676f6c8ecf09430f030b14d6c8f
itume <jem90ham@gmail.com>
2019-06-30 15:13:26 +0900

    change constant name

--- lib/fools_gold/tax.rb
+++ lib/fools_gold/tax.rb
@@ -1,9 +1,10 @@
 require 'date'
 class Tax
 
-  REDUCED_RATE = 8
-  NORMAL_RATE = 10
-  private_constant :REDUCED_RATE, :NORMAL_RATE
+  NORMAL_RATE_BEFORE_2019_OCT = 8
+  REDUCED_RATE_AFTER_2019_OCT = 8
+  NORMAL_RATE_AFTER_2019_OCT = 10
+  private_constant :NORMAL_RATE_BEFORE_2019_OCT, :REDUCED_RATE_AFTER_2019_OCT, :NORMAL_RATE_AFTER_2019_OCT
 
   def initialize
     @rate = current_rate
@@ -22,10 +23,10 @@ class Tax
   private
 
   def current_rate
-    TaxDate.day_of_raise_10_percent? ? NORMAL_RATE : REDUCED_RATE
+    TaxDate.day_of_raise_10_percent? ? NORMAL_RATE_AFTER_2019_OCT : NORMAL_RATE_BEFORE_2019_OCT
   end
 
   def current_r_rate
-    TaxDate.day_of_raise_10_percent? ? REDUCED_RATE : nil
+    TaxDate.day_of_raise_10_percent? ? REDUCED_RATE_AFTER_2019_OCT : nil
   end
 end
