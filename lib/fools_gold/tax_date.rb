f5a8e938591f4b74fcc61a793b45f71c70a6cc71
itume <jem90ham@gmail.com>
2019-07-01 22:57:52 +0900

    private

--- lib/fools_gold/tax_date.rb
+++ lib/fools_gold/tax_date.rb
@@ -1,17 +1,19 @@
 require 'date'
 module TaxDate
 
+  def day_of_raise_10_percent?
+    return true if Object.const_defined?('Settings') && Settings.row_inforced
+    today >= day_of_raise_10_percent
+  end
+
+  private
+
   def today
     Date.today
   end
 
   def day_of_raise_10_percent
     Date.new(2019, 10, 1)
-  end
-
-  def day_of_raise_10_percent?
-    return true if Object.const_defined?('Settings') && Settings.row_inforced
-    today >= day_of_raise_10_percent
   end
 
   module_function :today, :day_of_raise_10_percent, :day_of_raise_10_percent?
