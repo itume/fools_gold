1d6e9e814ca320170b9642cbed9a9ada5670a193
itume <jem90ham@gmail.com>
2019-07-01 23:53:20 +0900

    add tax_law_spec and mv testcase

--- spec/fools_gold_spec.rb
+++ spec/fools_gold_spec.rb
@@ -2,15 +2,4 @@ RSpec.describe FoolsGold do
   it "has a version number" do
     expect(FoolsGold::VERSION).not_to be nil
   end
-
-  describe "TaxLaw" do
-    it "設定変更を行うことで強制的に新税適用できること" do
-      class MyTaxLaw < TaxLaw
-        inforced_201910 true
-      end
-      allow(TaxDate).to receive(:today).and_return(Date.new(2019,9,30))
-      expect(Yen.new(100).with_tax).to eq(110)
-      expect(Yen.new(100).with_reduced_tax).to eq(108)
-    end
-  end
 end
