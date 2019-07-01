d0bce93f2b7508d16f633c545d4ee80a490849cc
itume <jem90ham@gmail.com>
2019-07-01 23:50:15 +0900

    thank you settingslogic

--- lib/fools_gold.rb
+++ lib/fools_gold.rb
@@ -4,7 +4,6 @@ require_relative 'fools_gold/tax'
 require_relative 'fools_gold/tax_date'
 require_relative 'fools_gold/errors'
 require_relative 'fools_gold/tax_law'
-require "settingslogic"
 
 
 module FoolsGold
